c0541b4a2c73e3f04e92c4c83f25d98fdee19a80
Mario Lang <mlang@blind.guru>
2025-07-07 21:46:53 +0200

    wip

--- llm_wikimedia.py
+++ llm_wikimedia.py
@@ -1,5 +1,6 @@
 from typing import Literal
 
+import click
 import httpx
 import llm
 from xml.etree import ElementTree
@@ -23,7 +24,7 @@ def wikimedia(page: str, lang: Lang = "en", site: Site = "wikipedia"):
     wikimedia = ElementTree.fromstring(response.text)
     p = wikimedia.find(f'{export}page')
     if p is None:
-        raise RuntimeError("There is no page with that name")
+        raise RuntimeError(f"There is no page named {page}")
     latest = p.find(f'{export}revision/{export}text')
     if latest is None:
         raise RuntimeError("Could not extract text of latest revision from XML")
@@ -38,3 +39,15 @@ def register_tools(register):
 def register_fragment_loaders(register):
     register("wikipedia", lambda article: llm.Fragment(wikimedia(article, "en", "wikipedia")))
     register("wiktionary", lambda article: llm.Fragment(wikimedia(article, "en", "wiktionary")))
+
+
+@click.command()
+@click.option('-l', '--language', default='en', show_default=True)
+@click.option('-s', '--site', default='wikipedia', show_default=True)
+@click.argument("page")
+def cli(page, site, language):
+    print(wikimedia(page, language, site))
+
+
+if __name__ == '__main__':
+    cli()
